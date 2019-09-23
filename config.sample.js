f4e5f13a8a2ffd97f26584aea3cf5150dc6695ec
Chris Sherman <chris.sherman@outlook.com>
2019-09-23 09:36:25 -0400

    Remove region property from config.

--- config.sample.js
+++ config.sample.js
@@ -10,24 +10,4 @@ module.exports = {
   propertySupplierZipCode: 'supplierZip',
   regexEmailAddress: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
   regexPhoneNumber: /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/,
-  regions: [
-    new Region(
-      'New River Valley',
-      new Set(['24068', '24073', '24061', '24060', '24141', '24142', '24143']),
-    ),
-    new Region(
-      'Roanoke Valley',
-      new Set([
-        '24011',
-        '24012',
-        '24013',
-        '24014',
-        '24015',
-        '24016',
-        '24017',
-        '24018',
-        '24019',
-      ]),
-    ),
-  ],
 };
